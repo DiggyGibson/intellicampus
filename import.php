7050db91ba07fdfacf3d340f0d1029e093a77527
Roosevelt Gibson <diggygibson.r@gmail.com>
2025-09-26 14:54:50 +0000

    Add database import script

---
+++ import.php
@@ -0,0 +1,7 @@
+﻿<?php
+$content = file_get_contents('intellicampus_schema.sql');
+if (substr($content, 0, 3) == "\xEF\xBB\xBF") {
+    $content = substr($content, 3);
+}
+file_put_contents('clean.sql', $content);
+echo shell_exec('PGPASSWORD=${DB_PASSWORD} psql -h ${DB_HOST} -U ${DB_USERNAME} -d ${DB_DATABASE} -p ${DB_PORT} -f clean.sql 2>&1');
